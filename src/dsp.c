6f494d62f4b0b78f8102fd80e3fc027cefa9051a
MajorX234 <majorx234@googlemail.com>
2024-11-27 01:59:25 +0100

    Add GNU License

--- src/dsp.c
+++ src/dsp.c
@@ -1,4 +1,26 @@
-#include <unistd.h>
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//    This file is part of jack_fft_visualizer                               //
+//    Copyright (C) 2024 <majorx234@googlemail.com>                          //
+//                                                                           //
+//    jack_fft_visualizer is free software:                                  //
+//    you can redistribute it and/or modify                                  //
+//    it under the terms of the GNU General Public License as published by   //
+//    the Free Software Foundation, either version 3 of the License, or      //
+//    (at your option) any later version.                                    //
+//                                                                           //
+//    jack_fft_visualizer is distributed in the hope that it will be useful, //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    GNU General Public License for more details.                           //
+//                                                                           //
+//    You should have received a copy of the GNU General Public License      //
+//    along with jack_fft_visualizer.                                        //
+//    If not, see <http://www.gnu.org/licenses/>.                            //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+/// #include <unistd.h>
 #include <math.h>
 #include "dsp.h"
 
--- src/simple_dft.c
+++ src/simple_dft.c
@@ -1,8 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//    This file is part of jack_fft_visualizer                               //
+//    Copyright (C) 2024 <majorx234@googlemail.com>                          //
+//                                                                           //
+//    jack_fft_visualizer is free software:                                  //
+//    you can redistribute it and/or modify                                  //
+//    it under the terms of the GNU General Public License as published by   //
+//    the Free Software Foundation, either version 3 of the License, or      //
+//    (at your option) any later version.                                    //
+//                                                                           //
+//    jack_fft_visualizer is distributed in the hope that it will be useful, //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    GNU General Public License for more details.                           //
+//                                                                           //
+//    You should have received a copy of the GNU General Public License      //
+//    along with jack_fft_visualizer.                                        //
+//    If not, see <http://www.gnu.org/licenses/>.                            //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
 #include "simple_dft.h"
 #include <assert.h>
 #include <math.h>
 #include <complex.h>
-#include <stdio.h>
 
 SimpleDFT* create_simple_dft(size_t size) {
   SimpleDFT* new_dft = (SimpleDFT*)malloc(sizeof(SimpleDFT));
